72046d4b951158eb82f429b2c0971c2ebbcbacef
Emmett Frett <emfrett@gmail.com>
2026-04-17 09:10:22 +0200

    Fix: Swap to modern next-pwa fork to resolve Edge middleware Webpack bug

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
